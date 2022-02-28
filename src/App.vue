c3c058d26413dd947bd3f9b256b95199cea64a30
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-02-27 23:25:16 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  /*Custom Properties*/
+  --rojoOscuro: #9a232c;
+  --rojoClaro: #bd252c;
+  --amarillo: #f2901a;
+  --negro: #1f1c1f;
+  --naranja: #e95824;
+  --azul: #0766a3;
+  --anchoPantalla: 1200px;
+}
+
+/*Scrollbar*/
+::-webkit-scrollbar {
+  width: 1.1rem;
+}
+
+::-webkit-scrollbar-track {
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 1) 95%,
+    rgba(154, 35, 44, 1) 100%
+  );
+}
+
+::-webkit-scrollbar-track:hover,
+::-webkit-scrollbar-track:active {
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 1) 95%,
+    rgba(242, 144, 26, 1) 100%
+  );
+}
+
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(transparent, #9a232c);
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb:hover,
+::-webkit-scrollbar-thumb:active {
+  background: linear-gradient(transparent, #f2901a);
 }
 </style>
